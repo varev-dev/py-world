609ecd627cc328e7b7c6c6241aa7b3392a007e8f
Kacper Doga <doga.kacper@gmail.com>
2024-06-11 00:43:49 +0200

    main restruct, test world and animal

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 import tkinter as tk
 from tkinter import Text
 
+from Organism import Organism
+from Animal import Animal
+from Position import Position
+from World import World
+
+
+def next_turn(wrd: World, rt: tk.Tk, cnv: tk.Canvas, text: tk.Text):
+    wrd.make_turn()
+    wrd.print(cnv, text)
+
+
 if __name__ == '__main__':
     root = tk.Tk()
     root.title("Py World")
+
+    world = World(10, 10, 2)
+    organism = Animal(world, Position(2, 2), 2, 2, 1, 'blue')
+    world.add_organism(organism)
 
     # Configure grid weights for proper stretching
     root.grid_columnconfigure(1, weight=1)
@@ -32,18 +47,19 @@ if __name__ == '__main__':
     btn_skill = tk.Button(button_frame, text="use skill")
     btn_next = tk.Button(button_frame, text="next round")
 
+    # Set up the text area
+    text_area = Text(root, height=45, width=45)
+    text_area.grid(row=1, column=1, columnspan=3, rowspan=19, padx=10, pady=10, sticky="nsew")
+
     # Arrange the buttons in a grid within the frame
     btn_new.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
     btn_save.grid(row=0, column=1, padx=5, pady=5, sticky="ew")
     btn_load.grid(row=0, column=2, padx=5, pady=5, sticky="ew")
     btn_skill.grid(row=1, column=0, columnspan=2, padx=5, pady=5, sticky="ew")
+
+    canvas.delete('all')
     btn_next.grid(row=1, column=2, padx=5, pady=5, sticky="ew")
-
-    # Set up the text area
-    text_area = Text(root, height=45, width=45)
-    text_area.grid(row=1, column=1, columnspan=3, rowspan=19, padx=10, pady=10, sticky="nsew")
-
-    # Insert sample text into the text area
+    btn_next.config(command=lambda: next_turn(world, root, canvas, text_area))
 
     # Start the Tkinter event loop
     root.mainloop()
